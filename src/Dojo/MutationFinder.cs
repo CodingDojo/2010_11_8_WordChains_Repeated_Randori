e55af03d8378f097f01e52210fca8906b8c4dc59
Peter Sacchet <webmaster@c-designed.com>
2010-11-08 19:33:46 +0100

    autocommit after successful build

---
+++ src/Dojo/MutationFinder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dojo
+{
+    class MutationFinder
+    {
+        internal List<string> GetAllMutations(string p, List<string> list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Dojo/MutationFinderTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Dojo
+{
+    [TestFixture]
+    public class MutationFinderTest
+    {
+        [Test]
+        public void GetAllMutations_EmptyDictionary_ReturnsEmptyResult()
+        {
+            MutationFinder finder = new MutationFinder();
+            List<string> result = finder.GetAllMutations("", new List<string>());
+
+            Assert.That(result.Count, Is.EqualTo(0));
+
+        }
+    }
+}
