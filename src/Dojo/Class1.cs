d9fd2e66603a08db962f540dee292b8366f30185
Peter Sacchet <webmaster@c-designed.com>
2010-11-08 19:14:16 +0100

    autocommit after successful build

---
+++ src/Dojo/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Dojo
+{
+    [TestFixture]
+    public class MutationCheckerTest
+    {
+        [Test]
+        public void IsMutation_IsValidMutation_ReturnsTrue()
+        {
+            MutaionChecker checker = new MutaionChecker();
+
+            bool result = checker.IsMutation("cog", "cot");
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
