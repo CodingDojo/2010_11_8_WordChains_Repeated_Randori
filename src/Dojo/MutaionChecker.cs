365144dc203120f76689cebf9fd6561842336786
Peter Sacchet <webmaster@c-designed.com>
2010-11-08 19:27:51 +0100

    autocommit after successful build

--- src/Dojo/MutaionChecker.cs
+++ src/Dojo/MutaionChecker.cs
@@ -10,11 +10,21 @@ namespace Dojo
         internal bool IsMutation(string p, string p_2)
         {
 
-
             if (p.Equals(p_2) || p.Length != p_2.Length)
                 return false;
 
-            return true;
+            int differences = 0;
+
+            for (int i = 0; i < p.Length; i++)
+            {
+                if (p[i] != p_2[i])
+                    differences++;
+            }
+
+            return differences == 1;
+
+            
+
         }
     }
 }
