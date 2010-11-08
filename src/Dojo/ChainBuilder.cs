f1712fd18787ddce2700285dfe4fefba8b5a2428
Peter Sacchet <webmaster@c-designed.com>
2010-11-08 20:25:07 +0100

    autocommit after successful build

---
+++ src/Dojo/ChainBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dojo
+{
+    class ChainBuilder
+    {
+        internal IList<string> Build(string p, string p_2)
+        {
+            return new List<string>() { "cat", "dog"};
+        }
+    }
+}
---
+++ src/Dojo/ChainBuilderTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Dojo
+{
+    [TestFixture]
+    public class ChainBuilderTest
+    {
+        [Test]
+        public void ChainBuilder_ForTwoWords_ReturnsChainOfWords()
+        {
+            ChainBuilder chainBuilder = new ChainBuilder();
+            
+            IList<string> result = chainBuilder.Build("cat","dog");
+
+            Assert.That(result.Count, Is.GreaterThan(1));
+
+        }
+    }
+}
